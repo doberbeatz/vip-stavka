90424bc921b6ff885cd3144858932430480745a0
Roman <doberbeatz@gmail.com>
2015-03-12 10:40:53 +0300

    Setup About Page

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -3,162 +3,119 @@
 @section('content')
     <section id="feature" class="transparent-bg">
         <div class="container">
-            <div class="get-started center wow fadeInDown">
-                <h2>Ready to get started</h2>
-                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore  magna aliqua. <br>  Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
-                <div class="request">
-                    <h4><a href="#">Request a free Quote</a></h4>
-                </div>
-            </div><!--/.get-started-->
 
-            <div class="row">
+            <div class="center wow fadeInDown">
+                <h2>Обо мне</h2>
+                <p class="lead">Я, <b>Жванецкий Антон Рудольфович</b>, являюсь основателем  «Группы Независимых Капперов»  Восточной Европы и стран СНГ. Хочется подчеркнуть Ваше внимание на слове  «Независимые». Проект  vip-stavka.com  является объединением художников каперского искусства, а я именно так отношусь к  своему любимому  делу.</p>
+            </div>
+
+            <div id="about-slider">
+                <div id="carousel-slider" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators visible-xs">
+                        <li data-target="#carousel-slider" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-slider" data-slide-to="1"></li>
+                        <li data-target="#carousel-slider" data-slide-to="2"></li>
+                    </ol>
+
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img src="/assets/images/slider_one.jpg" class="img-responsive" alt="">
+                        </div>
+                        <div class="item">
+                            <img src="/assets/images/slider_one.jpg" class="img-responsive" alt="">
+                        </div>
+                        <div class="item">
+                            <img src="/assets/images/slider_one.jpg" class="img-responsive" alt="">
+                        </div>
+                    </div>
+
+                    <a class="left carousel-control hidden-xs" href="#carousel-slider" data-slide="prev">
+                        <i class="fa fa-angle-left"></i>
+                    </a>
+
+                    <a class=" right carousel-control hidden-xs"href="#carousel-slider" data-slide="next">
+                        <i class="fa fa-angle-right"></i>
+                    </a>
+                </div> <!--/#carousel-slider-->
+            </div><!--/#about-slider-->
+
+            <div class="row ">
                 <div class="features">
                     <div class="col-md-4 col-sm-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
                         <div class="feature-wrap">
-                            <i class="fa fa-bullhorn"></i>
-                            <h2>Fresh and Clean</h2>
-                            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h3>
+                            <i class="fa fa-user"></i>
+                            <h2>Возраст</h2>
+                            <h3>32 года</h3>
                         </div>
                     </div><!--/.col-md-4-->
 
                     <div class="col-md-4 col-sm-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
                         <div class="feature-wrap">
-                            <i class="fa fa-comments"></i>
-                            <h2>Retina ready</h2>
-                            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h3>
+                            <i class="fa fa-male"></i>
+                            <h2>Семейное положение</h2>
+                            <h3>Не женат</h3>
                         </div>
                     </div><!--/.col-md-4-->
 
                     <div class="col-md-4 col-sm-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
                         <div class="feature-wrap">
-                            <i class="fa fa-cloud-download"></i>
-                            <h2>Easy to customize</h2>
-                            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h3>
+                            <i class="fa fa-flag"></i>
+                            <h2>Гражданство</h2>
+                            <h3>Являюсь гражданином ЕС</h3>
                         </div>
                     </div><!--/.col-md-4-->
 
                     <div class="col-md-4 col-sm-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
                         <div class="feature-wrap">
-                            <i class="fa fa-leaf"></i>
-                            <h2>Adipisicing elit</h2>
-                            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h3>
+                            <i class="fa fa-cloud"></i>
+                            <h2>Вероисповедание</h2>
+                            <h3>Атеист</h3>
                         </div>
                     </div><!--/.col-md-4-->
 
                     <div class="col-md-4 col-sm-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
                         <div class="feature-wrap">
                             <i class="fa fa-cogs"></i>
-                            <h2>Sed do eiusmod</h2>
-                            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h3>
+                            <h2>Мотивация</h2>
+                            <h3>Верю в цифры и в себя</h3>
                         </div>
                     </div><!--/.col-md-4-->
 
                     <div class="col-md-4 col-sm-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
                         <div class="feature-wrap">
                             <i class="fa fa-heart"></i>
-                            <h2>Labore et dolore</h2>
-                            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h3>
+                            <h2>Предпочтение</h2>
+                            <h3>Люблю  порядок во всем</h3>
                         </div>
                     </div><!--/.col-md-4-->
                 </div><!--/.services-->
             </div><!--/.row-->
 
-            <section id="content">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-8 wow fadeInDown">
-                            <div class="tab-wrap">
-                                <div class="media">
-                                    <div class="parrent pull-left">
-                                        <ul class="nav nav-tabs nav-stacked">
-                                            <li class=""><a href="#tab1" data-toggle="tab" class="analistic-01">Responsive Web Design</a></li>
-                                            <li class="active"><a href="#tab2" data-toggle="tab" class="analistic-02">Premium Plugin Included</a></li>
-                                            <li class=""><a href="#tab3" data-toggle="tab" class="tehnical">Predefine Layout</a></li>
-                                            <li class=""><a href="#tab4" data-toggle="tab" class="tehnical">Our Philosopy</a></li>
-                                            <li class=""><a href="#tab5" data-toggle="tab" class="tehnical">What We Do?</a></li>
-                                        </ul>
-                                    </div>
+            <div class="get-started center wow fadeInDown">
+                <p class="lead text-justify">Практически всю сознательную жизнь прожил на территории ГДР ( бывшей ), ныне ФРГ а именно  в городе Дрезден.  В данный момент проживаю на территории Респ. Беларусия в частном доме на окраине города Бреста.  Потянуло так скажем на историческую Родину.  Часто езжу по территории современной РФ. В долгих своих поездках начал изучать рынок  «профи каперов» на пост советском пространстве. Если честно я ужаснулся от  увиденного, поверьте мне есть с чем сравнивать. В Германии например, чтобы называть себя профессиональным капером нужно пройти жесточайшую аттестацию и заплатить кучу налогов, чтобы только начать свое дело. Здесь как Вы знаете не нужно ничего, любой проходимец может открыть свой сайт и начать своё «дело».  Я сделал у каждого капера входящего в  топ 10 прогнозистов  СНГ по одному прогнозу и ВЫ не поверите из 10 прогнозов, которые  они предоставили  - зашло только 2!!!! Да именно всего 2 прогноза из 10 .  Далее потом этих двоих, которые были чуть успешнее своих коллег, на второй прогноз просто не хватило.   Я  думал, что будут минусы но не так  из 12 прогнозов от лучших, я подчеркиваю, что это элита каперского искусства СНГ -  удачными оказались только 2 прогноза. И тут я понял, что  конкуренции просто никакой здесь для меня нету! Я с детских лет увлекаюсь спортом, а именно ввиду свою статистику всех крупных спортивных событий.  Реальный опыт игры с букмекерами на деньги более 10 лет. Букмекеров можно обыграть и нужно это делать, но нужен определенный подход.   И этот так называемый подход у меня есть. Примерно 12 лет назад на основании своих наблюдений  Я  сделал первую ставку. Старт в игре дался нелегко: была серия проигрышей, волнение и азарт мешали сделать нужную ставку. Были опробованы множество методик, вариантов и спустя время разработаны свои методы. Благодаря пройденному появился ценный опыт и прибыль. В 2013 году, осуществляя регулярные выигрышные ставки, ко мне приходит идея личного капперского центра, нацеленного на помощь в избежание типичных ошибок тем кто хочет зарабатывать на этом.   Со временем Я  собираю вокруг себя команду профессионалов, отлично разбирающихся в спорте и имеющих опыт личных ставок в букмекерских конторах ( в основном  коренные немцы и этнические турки). Капперы, которые работают со мной прошли жесткий отбор, показали и показывают себя с хорошей стороны.</p><p>Заказы осуществляются через электронную почту vip-stavka@rambler.ru (круглосуточно). Звонки принимаются по номеру +7 (495) 100-20-30</p>
+                <div class="request">
+                    <h4><a href="#">vip-stavka@rambler.ru</a></h4>
+                </div>
+            </div><!--/.get-started-->
 
-                                    <div class="parrent media-body">
-                                        <div class="tab-content">
-                                            <div class="tab-pane fade" id="tab1">
-                                                <div class="media">
-                                                    <div class="pull-left">
-                                                        <img class="img-responsive" src="/assets/images/tab2.png">
-                                                    </div>
-                                                    <div class="media-body">
-                                                        <h2>Adipisicing elit</h2>
-                                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use.</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div class="tab-pane fade active in" id="tab2">
-                                                <div class="media">
-                                                    <div class="pull-left">
-                                                        <img class="img-responsive" src="/assets/images/tab1.png">
-                                                    </div>
-                                                    <div class="media-body">
-                                                        <h2>Adipisicing elit</h2>
-                                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use.
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div class="tab-pane fade" id="tab3">
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
-                                            </div>
-
-                                            <div class="tab-pane fade" id="tab4">
-                                                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words</p>
-                                            </div>
-
-                                            <div class="tab-pane fade" id="tab5">
-                                                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures,</p>
-                                            </div>
-                                        </div> <!--/.tab-content-->
-                                    </div> <!--/.media-body-->
-                                </div> <!--/.media-->
-                            </div><!--/.tab-wrap-->
-                        </div><!--/.col-sm-6-->
-
-                        <div class="col-xs-12 col-sm-4 wow fadeInDown">
-                            <div class="testimonial">
-                                <h2>Testimonials</h2>
-                                <div class="media testimonial-inner">
-                                    <div class="pull-left">
-                                        <img class="img-responsive img-circle" src="/assets/images/testimonials1.png">
-                                    </div>
-                                    <div class="media-body">
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
-                                        <span><strong>-John Doe/</strong> Director of corlate.com</span>
-                                    </div>
-                                </div>
-
-                                <div class="media testimonial-inner">
-                                    <div class="pull-left">
-                                        <img class="img-responsive img-circle" src="/assets/images/testimonials1.png">
-                                    </div>
-                                    <div class="media-body">
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
-                                        <span><strong>-John Doe/</strong> Director of corlate.com</span>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-
-                    </div><!--/.row-->
-                </div><!--/.container-->
-            </section><!--/#content-->
+            <div class="row">
+                <div class="col-md-4 wow fadeInDown">
+                    <p class="text-justify">После оформления заказа с клиентом связывается наш менеджер и высылает реквизиты компании для оплаты. Затем после осуществления платежа клиент сообщает сумму и точное время оплаты, добавив в сообщении чек, принтскрин или фото документа об оплате. После проверки факта поступления денег, ему отправляется прогноз.
+                    </p>
+                </div>
+                <div class="col-md-4 wow fadeInDown">
+                    <p class="text-justify">Я не от кого не скрываюсь, в любой момент ВЫ можете позвонить мне и мы все обсудим.  Я гарантирую Вам профессионализм во всем, настоящее европейское качество по вопросам каперства.
+                    </p>
+                </div>
+                <div class="col-md-4 wow fadeInDown">
+                    <p class="text-justify">Я   внимательный  ко всем заказчикам и выполняем услуги на взаимовыгодных условиях. Более 1000 клиентов по всей России и СНГ, а также стран ближайшего зарубежья уже убедились в качестве моих прогнозов!
+                    </p>
+                </div>
+            </div>
 
             <section id="recent-works">
                 <div class="container">
-                    <div class="center wow fadeInDown">
-                        <h2>Recent Works</h2>
-                        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut <br> et dolore magna aliqua. Ut enim ad minim veniam</p>
-                    </div>
 
                     <div class="row">
                         <div class="col-xs-12 col-sm-4 col-md-3">
